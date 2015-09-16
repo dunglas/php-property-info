0cc9865337ac179f71f2c4e754d7dbb825dbcd91
Kévin Dunglas <dunglas@gmail.com>
2015-09-16 23:55:51 +0200

    PHP 7 types support.

---
+++ src/PropertyInfo/Tests/Fixtures/Php7Dummy.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PropertyInfo\Tests\Fixtures;
+
+/**
+ * @author Kévin Dunglas <dunglas@gmail.com>
+ */
+class Php7Dummy
+{
+    public function getFoo(): array
+    {
+    }
+
+    public function setBar(int $bar)
+    {
+    }
+
+    public function addBaz(string $baz)
+    {
+    }
+}
